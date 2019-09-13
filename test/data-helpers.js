368453fea4f430461d678f266f00f52628924708
Jackmc <jackmc1122@gmail.com>
2019-09-12 17:39:18 -0700

    Get verify route working

--- test/data-helpers.js
+++ test/data-helpers.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
 
 beforeAll(() => {
   connect();
@@ -10,6 +12,19 @@ beforeEach(() => {
   return mongoose.connection.dropDatabase();
 });
 
+let agent = request.agent(app);
+let token = null;
+beforeEach(async() => {
+  return await agent
+    .post('/api/v1/auth/guest')
+    .send({ nickname: 'guestuser', avatar: 'someimgurl' });
+});
+
 afterAll(() => {
   return mongoose.connection.close();
 });
+
+module.exports = {
+  getAgent: () => agent,
+  getToken: () => token
+};
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,4 +1,4 @@
-require('../data-helpers');
+const { getAgent } = require('../data-helpers');
 const request = require('supertest');
 const app = require('../../lib/app');
 
@@ -6,13 +6,27 @@ describe('auth routes', () => {
   it('creates and returns a user', () => {
     return request(app)
       .post('/api/v1/auth/guest')
-      .send({ nickname: 'jack', isGuest: true, avatar: 'someimgurl' })
+      .send({ nickname: 'jack', avatar: 'someimgurl' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           nickname: 'jack',
           isGuest: true,
           avatar: 'someimgurl',
+          __v: 0
+        });
+      });
+  });
+
+  it('can verify that a user is signed in', () => {
+    return getAgent()
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          nickname: 'guestuser',
+          avatar: 'someimgurl',
+          isGuest: true,
           __v: 0
         });
       });
