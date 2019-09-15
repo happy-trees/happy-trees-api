4a0e1993157f803fa783ed3391a506b534ac8e73
Jack McConnell <jackmc1122@gmail.com>
2019-09-15 11:49:48 -0700

    test submitting a correct answer

--- test/routes/socket.test.js
+++ test/routes/socket.test.js
@@ -1,14 +1,19 @@
-const { getToken } = require('../data-helpers');
+const { getToken, getToken2, getUserId } = require('../data-helpers');
 const io = require('socket.io-client');
 const http = require('../../lib/app');
 
 describe('auth routes', () => {
   let socket1;
   let socket2;
+
+  let userId;
   beforeEach(async() => {
     http.listen(3001);
 
     const token = getToken();
+    const token2 = getToken2();
+
+    userId = getUserId();
 
     socket1 = io.connect('http://localhost:3001', {
       extraHeaders: { Cookie: token },
@@ -19,7 +24,7 @@ describe('auth routes', () => {
     });
 
     socket2 = io.connect('http://localhost:3001', {
-      extraHeaders: { Cookie: token },
+      extraHeaders: { Cookie: token2 },
       'reconnection delay' : 0, 
       'reopen delay' : 0, 
       'force new connection' : true, 
@@ -27,7 +32,7 @@ describe('auth routes', () => {
     });
   });
 
-  afterAll(() => {
+  afterEach(() => {
     http.close();
   });
 
@@ -56,6 +61,27 @@ describe('auth routes', () => {
       color: '#000000',
       strokeWidth: 5
     };
+    socket1.emit('find game');
+    setTimeout(() => {
+      socket2.emit('find game');
+    }, 100);
+  });
+
+  it('starts a game and and sends a correct answer', (done) => {
+    socket2.on('correct answer', ({ isCorrect }) => {
+      expect(isCorrect).toEqual(true);
+      socket1.close();
+      socket2.close();
+      done();
+    });
+    socket1.on('start game', (startRound) => {
+      socket1.emit('answer', { 
+        answer: 'trees', 
+        roundId: startRound._id, 
+        gameId: startRound.gameId,
+        userId 
+      });
+    });
     socket1.emit('find game');
     setTimeout(() => {
       socket2.emit('find game');
